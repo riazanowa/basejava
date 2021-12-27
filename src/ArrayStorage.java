2a78bb124df883e869aedcccebfefa20af3295a2
Ekaterina Ryazanova <kat.riazanowa@yandex.ru>
2021-12-27 17:01:11 +0300

    Improvements for 1 task

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,19 +5,20 @@ import static java.util.Arrays.copyOf;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) return storage[i];
         }
         return null;
@@ -25,30 +26,24 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int i;
-        for (i = 0; i < size(); i++) {
+        for (i = 0; i < size; i++) {
             if (uuid.equals(storage[i].toString())) break;
         }
 
-        for (int j = i; j <= size() - 2; j++) {
+        for (int j = i; j < size - 1; j++) {
             storage[j] = storage[j + 1];
         }
+        storage[--size] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return copyOf(storage, size());
+        return copyOf(storage, size);
     }
 
     int size() {
-        int count = 0;
-        for (int i = 0; i < storage.length; count++) {
-            if (storage[i] == null) {
-                break;
-            }
-            count++;
-        }
-        return count;
+        return size;
     }
 }
