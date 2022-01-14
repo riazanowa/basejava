12bbdc8188d0cf16bc085a7f96987597012fe6be
Ekaterina Ryazanova <kat.riazanowa@yandex.ru>
2022-01-14 11:07:28 +0300

    HW2 - fixies3

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -18,22 +18,22 @@ public class ArrayStorage {
 
     public void update(Resume r) {
         int index = getIndex(r.getUuid());
-        if (index == -1) {
-            System.out.println("Not updated. Resume " + r.getUuid() + " doesn't exist.");
-        } else {
+        if (index != -1) {
             storage[index] = r;
             System.out.println("Resume " + r.getUuid() + " has been updated successfully.");
+        } else {
+            System.out.println("Not updated. Resume " + r.getUuid() + " doesn't exist.");
         }
     }
 
     public void save(Resume r) {
         if (size == storage.length) System.out.println("Can't be saved. The storage is full");
 
-        if (getIndex(r.getUuid()) == -1) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Not saved. Resume " + r.getUuid() + " is already present.");
+        } else {
             storage[size++] = r;
             System.out.println("Resume " + r.getUuid() + " has been saved.");
-        } else {
-            System.out.println("Not saved. Resume " + r.getUuid() + " is already present.");
         }
     }
 
@@ -43,15 +43,16 @@ public class ArrayStorage {
         if (index != -1) {
             return storage[index];
         } else {
+            System.out.println("Resume " + uuid + " doesn't exists.");
             return null;
         }
     }
 
     public void delete(String uuid) {
-        int i = getIndex(uuid);
+        int index = getIndex(uuid);
 
-        if (i != -1) {
-            if (size - 1 - i >= 0) System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
+        if (index != -1) {
+            if (size - 1 - index >= 0) System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
             storage[--size] = null;
             System.out.println("Resume " + uuid + " has been deleted");
         } else {
@@ -70,13 +71,8 @@ public class ArrayStorage {
         return size;
     }
 
-    public boolean isPresent(String uuid) {
-        return getIndex(uuid) != -1;
-    }
-
-    public int getIndex(String uuid) {
-        int i;
-        for (i = 0; i < size; i++) {
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
