02f5fc23c32dcddda049438a7ca236d1414baa92
Ekaterina Ryazanova <kat.riazanowa@yandex.ru>
2022-12-28 23:44:12 +0300

    HW11 -improvement 1

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -4,7 +4,7 @@ public class MainConcurrency {
     private static Object ACCOUNT_1 = new Object();
     private static Object ACCOUNT_2 = new Object();
 
-    public static void  deposit(Object account, int amount) {
+    public static void deposit(Object account, int amount) {
         System.out.println(Thread.currentThread().getName() + " deposit()");
         try {
             Thread.sleep(500);
@@ -14,29 +14,32 @@ public class MainConcurrency {
         System.out.println("End: deposit()");
     }
 
-    public static void  withdraw(Object account, int amount) {
+    public static void withdraw(Object account, int amount) {
         System.out.println(Thread.currentThread().getName() + " withdraw()");
         System.out.println("End: withdraw()");
     }
-    public static void main(String[] args) {
-        Thread t1 = new Thread(() -> {
-            synchronized (ACCOUNT_1) {
-                deposit(ACCOUNT_1, 50);
-                synchronized (ACCOUNT_2) {
-                    withdraw(ACCOUNT_2, 50);
-                }
-            }
-        });
 
-        Thread t2 = new Thread(() -> {
-            synchronized (ACCOUNT_2) {
-                withdraw(ACCOUNT_2, 20);
-                synchronized (ACCOUNT_1) {
-                    deposit(ACCOUNT_1, 20);
-                }
-            }
-        });
+    public static void main(String[] args) {
+
+        int amount1 = 50;
+        int amount2 = 20;
+        Thread t1 = createTask(ACCOUNT_1, ACCOUNT_2, amount1, (Object acc1, int amount) -> deposit(acc1, amount), (Object acc, int amount) -> withdraw(acc, amount));
+        Thread t2 = createTask(ACCOUNT_2, ACCOUNT_1, amount2, (Object acc, int amount) -> withdraw(acc, amount), (Object acc, int amount) -> deposit(acc, amount));
         t1.start();
         t2.start();
     }
+
+    public static Thread createTask(Object acc1, Object acc2, int amount, CustomConsumer c1, CustomConsumer c2) {
+        return new Thread(() -> {
+            synchronized (acc1) {
+                c1.accept(acc1, amount);
+                synchronized (acc2) {
+                    c2.accept(acc2, amount);
+                }
+            }
+        });
+    }
+}
+interface CustomConsumer {
+    void accept(Object acc, int amount);
 }
